2dfda33c91e249942aa41c714c39ca4f0188341c
Hao Li <yfzm1223@sjtu.edu.cn>
2020-05-13 17:16:26 +0800

    [musl-libc] use ocall in read and write

--- lib/musl-1.1.18/src/unistd/read.c
+++ lib/musl-1.1.18/src/unistd/read.c
@@ -4,5 +4,6 @@
 
 ssize_t read(int fd, void *buf, size_t count)
 {
-	return syscall_cp(SYS_read, fd, buf, count);
+	// return syscall_cp(SYS_read, fd, buf, count);
+	return ocall_syscall3(SYS_read, fd, buf, count);
 }
--- lib/musl-1.1.18/src/unistd/write.c
+++ lib/musl-1.1.18/src/unistd/write.c
@@ -4,5 +4,6 @@
 
 ssize_t write(int fd, const void *buf, size_t count)
 {
-	return syscall_cp(SYS_write, fd, buf, count);
+	// return syscall_cp(SYS_write, fd, buf, count);
+	return ocall_syscall3(SYS_write, fd, buf, count);
 }
